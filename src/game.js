f874f57aab1404044abcdf5a4ffa6836f16de460
petertran410 <nhantran4102002@gmail.com>
2023-05-25 01:12:20 +0700

    Update: Fixing undo function
    There is a bug. When press button " ` " after cleaning a line then undo function happens but it will clean all the cubes before and keep the last drop. (UNKNOWN WHY)

--- src/game.js
+++ src/game.js
@@ -14,11 +14,12 @@ export default class Game {
   _topOut = false;
   _activePiece = null;
   _nextPiece = null;
-  _gameStatesStack = [];
+  // _gameStatesStack = [];
 
   constructor(rows, columns) {
     this._playfield = new Playfield(rows, columns);
     this._updatePieces();
+    this._gameStatesStack = [];
   }
 
   get level() {
@@ -95,7 +96,34 @@ export default class Game {
     }
   }
 
+  undo() {
+    if (this._gameStatesStack.length > 1) {
+      // Restore the previous game state from the stack
+      const currentState = this._gameStatesStack.pop();
+      const previousState = this._gameStatesStack[this._gameStatesStack.length - 1];
 
+      // Restore the game properties
+      this._score = previousState.score;
+      this._lines = previousState.lines;
+      this._topOut = previousState.topOut;
+      this._activePiece = previousState.activePiece;
+      this._nextPiece = previousState.nextPiece;
+      this._playfield = previousState.playfield;
+
+      // Update the pieces' positions
+      this._activePiece.x = previousState.activePiece.x;
+      this._activePiece.y = previousState.activePiece.y;
+      this._nextPiece.x = previousState.nextPiece.x;
+      this._nextPiece.y = previousState.nextPiece.y;
+
+      // Clear the playfield and relock the pieces
+      this._playfield.reset();
+      this._updatePlayfield();
+
+      // Update the view
+      this._updateView();
+    }
+  }
 
   _update() {
     // Update the game state
@@ -106,10 +134,28 @@ export default class Game {
     if (this._playfield.hasCollision(this._activePiece)) {
       this._topOut = true;
     }
+
+    const currentState = {
+      score: this._score,
+      lines: this._lines,
+      topOut: this._topOut,
+      activePiece: this._activePiece,
+      nextPiece: this._nextPiece,
+      playfield: this._playfield,
+    };
+    this._gameStatesStack.push(currentState);
   }
 
   _updatePlayfield() {
     this._playfield.lockPiece(this._activePiece);
+
+    // Lock the active piece and restore the playfield
+    this._playfield.lockPiece(this._activePiece);
+    for (let block of this._playfield) {
+      if (block && block !== this._activePiece) {
+        this._playfield[block.y][block.x] = block;
+      }
+    }
   }
 
   _updateScore() {
@@ -128,5 +174,50 @@ export default class Game {
       (this._playfield.columns - this._activePiece.width) / 2
     );
     this._activePiece.y = -1;
+  }
+
+  _updateView() {
+    // Clear the previous view
+    this._clearView();
+
+    // Render the playfield
+    for (let y = 0; y < this._playfield.length; y++) {
+      for (let x = 0; x < this._playfield[y].length; x++) {
+        const block = this._playfield[y][x];
+        if (block) {
+          this._renderBlock(x, y, block.color);
+        }
+      }
+    }
+
+    // Render the active piece
+    for (let block of this._activePiece.blocks) {
+      const x = this._activePiece.x + block.x;
+      const y = this._activePiece.y + block.y;
+      this._renderBlock(x, y, this._activePiece.color);
+    }
+
+    // Render the next piece
+    for (let block of this._nextPiece.blocks) {
+      const x = this._nextPiece.x + block.x;
+      const y = this._nextPiece.y + block.y;
+      this._renderBlock(x, y, this._nextPiece.color);
+    }
+
+    // Render the score and lines
+    // Replace the following lines with your specific rendering logic
+    console.log('Score:', this._score);
+    console.log('Lines:', this._lines);
+  }
+
+  _clearView() {
+    // Replace this method with your specific view clearing logic
+    // For example, if you're rendering on a canvas, you would clear the canvas here
+  }
+
+  _renderBlock(x, y, color) {
+    // Replace this method with your specific block rendering logic
+    // For example, if you're rendering on a canvas, you would draw a colored rectangle here
+    console.log('Render block at', x, y, 'with color', color);
   }
 }
