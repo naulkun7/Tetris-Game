44d00c4eca14d13390d5e9a927bdf7d91a465bf4
ThienPhuc1802 <91868450+thienphuc1802@users.noreply.github.com>
2023-05-28 23:43:57 +0700

    Update SortFunction

--- src/view.js
+++ src/view.js
@@ -187,10 +187,23 @@ export default class View {
       this.height / 2 + 96
     );
   }
+  bubbleSort(arr) {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+      for (let j = 0; j < len - i - 1; j++) {
+        if (arr[j]._score < arr[j + 1]._score) {
+          let tmp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = tmp;
+        }
+      }
+    }
+    return arr;
+  }
 
   renderEndScreen({ scoreArr }) {
     this._clearScreen();
-    scoreArr.sort((a, b) => b._score - a._score);
+    this.bubbleSort(scoreArr);
     this.context.fillStyle = "white";
     this.context.font = '15px "Press Start 2P"';
     this.context.textAlign = "center";
